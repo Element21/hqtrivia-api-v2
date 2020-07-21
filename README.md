c67c746e4b622d56b3a3b4c45a8eb988eabd8694
Aiden Marchiori <aiden@alanmarian.com>
2020-07-21 17:14:32 -0400

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,143 @@
+### ⚠ WARNING: I will be constantly updating stuff and some features might not work yet ⚠ 
+Please open an issue if you find any broken stuff
+
+## HQ Trivia API
+This is a Node.JS wrapper for HQ Trivia
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
+[![forthebadge](https://forthebadge.com/images/badges/designed-in-ms-paint.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/as-seen-on-tv.svg)](https://forthebadge.com)
+
+## Installation
+`npm i hqtrivia-api`
+
+## API Methods
+- `hq.getUserData()` - Get the authenticated user data
+- `hq.getShows()` - Get the game schedule
+- `hq.getLeaderboard()` - Get the game leaderboard
+- `hq.getUserById()` - Get user by ID
+- `hq.searchUsers()` - Search users
+- `hq.getFriends()` - Get all friends
+- `hq.acceptFriendRequest(userId)` - Accept friend request
+- `hq.addFriend(userId)` - Add friend
+- `hq.getIncomingFriendRequests()` - Get incoming friend requests
+- `hq.connectToGame()` - Connect to the game
+- `hq.disconnectFromGame()` - Disconnect from the game
+- `hq.getUpcomingSchedule()` - Get upcoming games schedule
+- `hq.setToken(token)` - Sets token for requests
+- `hq.changeUsername(username)` - Change username
+- `hq.checkUsername(username)` - Check username
+- `hq.makePayout(email)` - Make payout (Have not tested. I think hq uses a captcha for payouts. WILL FIX LATER)
+
+## Registration methods
+- `hq.sendCode(phone, [method])` - Sends the code to the specified phone
+- `hq.confirmCode(code, [verificationId])` - Confirm code
+- `hq.register(username, [referral], [verificationId])` - Register an account if it is not registered
+
+
+## Trivia Game Methods
+- `hq.sendAnswer(answerID, questionId)` - Send answer to HQ
+- `hq.sendSurveyAnswer(answerID, questionId)` - Send survey answer to HQ
+- `hq.useExtralive(questionId)` - Use extra live
+- `hq.sendEraser(questionId)` - Use eraser
+- `hq.getErasers(friendIds)` - Get erasers
+
+
+## Words Game Methods
+- `hq.sendLetter(roundId, showId, letter)` - Send letter to HQ
+- `hq.sendWord(roundId, showId, word)` - Send word to HQ
+
+## Events
+- `connected` - Called when successfully connected to the game (Words, Trivia)
+- `disconnected` - Called when disconnected from the game (Words, Trivia)
+- `question` - Called when a question is received from the server (Trivia)
+- `questionClosed` - Called when a question is closed (Trivia)
+- `questionSummary` - Called when the summary of a question is received from the server (Trivia)
+- `questionFinished` - Called when question is finished (Trivia)
+- `gameStatus` - Called when the game status is received from the server (Words, Trivia)
+- `startRound` - Called when the round starts (Words)
+- `letterReveal` - Called when letter reveal (Words)
+- `endRound` - Called when round ends (Words)
+- `showWheel` - Called when wheel shows (Words)
+- `hideWheel` - Called when wheel hideens (Words)
+- `guessResponse` - Called after sending a letter or word (Words)
+
+## Trivia Example 1
+```js
+const HQTrivia = require('hqtrivia-api')
+const hq = new HQTrivia('[token]')
+
+hq.connectToGame()
+
+hq.on('connected', () => {
+    console.log('Connected to HQ WS')
+})
+
+hq.on('question', (data) => { 
+    console.log(`Question #${data.questionNumber}/${data.questionCount}`) // Question #3/12
+    console.log(data.question) // In a jazz band, which instrument would be in the rhythm section?
+    console.log(data.answers.map(answer => answer.text).join(' | ')) // Trombone | Guitar | Violin | 
+
+    hq.sendAnswer(data.answers[1].answerId, data.questionId) // Sends the answer "Guitar"
+})
+
+hq.on('disconnected', (code) => {
+    console.log('Disconnected from HQ WS')
+})
+```
+
+## Words Example 1
+```js
+const HQTrivia = require('hqtrivia-api')
+const hq = new HQTrivia('[token]')
+
+hq.connectToGame()
+
+hq.on('connected', () => {
+    console.log('Connected to HQ WS')
+})
+
+hq.on('startRound', (data) => { 
+    console.log(`Round Number #${data.roundNumber}/${data.totalRounds}`) // Round Number #9/10
+    console.log(`Hint: ${data.hint}`) // Hint: Circus Performance
+    console.log(`Puzzle: `) // Puzzle: 
+    console.log(data.puzzleState.join(' | ')) // ******** | ******** | C********
+
+    hq.sendWord(data.roundId, data.showId, 'JUGGLING') // Send the letters "J", "U", "G", ...etc
+    hq.sendWord(data.roundId, data.showId, 'MULTIPLE') // Send the letters "M", "U", "L", ...etc
+    hq.sendWord(data.roundId, data.showId, 'CHAINSAWS') // Send the letters "C", "H", "I", ...etc
+    
+    // or
+    
+    hq.sendLetter(data.roundId, data.showId, 'J') // Send the letter "J"
+    hq.sendLetter(data.roundId, data.showId, 'U') // Send the letter "U"
+    hq.sendLetter(data.roundId, data.showId, 'G') // Send the letter "G"
+}) 
+
+hq.on('disconnected', (code) => {
+    console.log('Disconnected from HQ WS')
+})
+```
+## Register example
+```js
+const HQTrivia = require('hqtrivia-api')
+const hq = new HQTrivia()
+
+await hq.sendCode('+11111111111')
+const response = await hq.confirmCode('0228')
+if (response.accountRegistred) {
+    console.log(`token: ${response.token}`)
+    hq.setToken(response.token)
+} else {
+    const token = await hq.register('jsoplox', 'machuita')
+    console.log(`token: ${token}`)
+    hq.setToken(token)
+}
+```
+## Todo
+- Get link to video when joining game
+- Add a "onGame" event to wait until a game starts
+- Add chat support
+- Add referral codes to account after logging in
+- Update some ws stuff
+- Add some EPIC tests
---
+++ src/index.js
@@ -0,0 +1,367 @@
+const axios = require('axios')
+const {EventEmitter} = require('events')
+const WebSocket = require('ws')
+
+class HQTrivia extends EventEmitter {
+    constructor(token = '', apiURL = 'https://api-quiz.hype.space') {
+        super()
+        this.token = token
+        this.headers = {
+            'x-hq-client': 'Android/1.49.9',
+            'x-hq-country': 'US',
+            'x-hq-lang': 'en',
+            'x-hq-timezone': Intl.DateTimeFormat().resolvedOptions().timeZone,
+            ...this.token ? {'authorization': `Bearer ${token}`} : {}
+        }
+        this.axios = axios.create({
+            baseURL: apiURL,
+            headers: this.headers,
+            validateStatus: false
+        })
+        this.lastQuestion = {}
+    }
+
+    setToken(token) {
+        this.headers = {
+            'x-hq-client': 'Android/1.49.9',
+            'x-hq-country': 'US',
+            'x-hq-lang': 'en',
+            'x-hq-timezone': Intl.DateTimeFormat().resolvedOptions().timeZone,
+            'authorization': `Bearer ${token}`
+        }
+        this.token = token
+    }
+
+    async sendCode(phone, method = 'sms') {
+        const sendCodeRes = await this.axios.post('/verifications', {
+            phone: phone,
+            method: method
+        })
+
+        this.verificationId = sendCodeRes.data.verificationId
+        return {
+            success: !!sendCodeRes.data.verificationId,
+            verificationId: sendCodeRes.data.verificationId
+        }
+    }
+
+    async confirmCode(code, verificationId = this.verificationId) {
+        const confirmCodeRes = await this.axios.post(`verifications/${verificationId}`, {
+            code: code
+        })
+
+        if (confirmCodeRes.data.auth === null) {
+            return {
+                success: true,
+                accountRegistred: false
+            }
+        } else if (confirmCodeRes.data.accessToken) {
+            return {
+                success: true,
+                accountRegistred: true,
+                token: confirmCodeRes.data.accessToken
+            }
+        } else if (confirmCodeRes.data.auth.accessToken) {
+            return {
+                success: true,
+                accountRegistred: true,
+                token: confirmCodeRes.data.auth.accessToken
+            }
+        } else {
+            throw new Error('Unknown API error')
+        }
+    }
+
+    async register(username, referral = null, verificationId = this.verificationId) {
+        const registerRes = await this.axios.post('/users', {
+            country: 'MQ==',
+            language: 'us',
+            referringUsername: referral,
+            username: username,
+            verificationId: verificationId
+        })
+
+        switch (registerRes.data.errorCode) {
+            case 101:
+                throw new Error('Username already registred')
+
+            case 471:
+                throw new Error('Username too long')
+        }
+
+        if (registerRes.data.accessToken) {
+            return registerRes.data.accessToken
+        } else {
+            throw new Error('Unknown API Error')
+        }
+    }
+
+    async getUserData() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const userDataRes = await this.axios.get('/users/me')
+        if (userDataRes.data.error) throw new Error(userDataRes.data.error)
+        return userDataRes.data
+    }
+
+    async getConfig() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const configDataRes = await this.axios.get('/config')
+        if (userDataRes.data.error) throw new Error(userDataRes.data.error)
+        return configDataRes.data
+    }
+
+    async getShows() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const shows = await this.axios.get('shows/schedule')
+        return shows.data
+    }
+
+    async getLeaderboard() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const leaderboard = await this.axios.get('/users/leaderboard')
+        return leaderboard.data
+    }
+
+    async getUserById(id) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const userInfo = await this.axios.get(`/users/${id}`)
+        return userInfo.data
+    }
+
+    async getPayoutsInfo() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const payoutsInfo = await this.axios.get('/users/me/payouts')
+        return payoutsInfo.data
+    }
+
+    async makePayout(email) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const makePayout = await this.axios.post('/users/me/payouts', {
+            email: email
+        })
+        return makePayout.data
+    }
+
+    async changeUsername(username) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const changeUsernameResp = await this.axios.patch('/users/me', {
+            username: username
+        })
+        return changeUsernameResp.data
+    }
+
+    async checkUsername(username) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const checkUsernameResp = await this.axios.post('/usernames/available', {
+            username: username
+        })
+        return checkUsernameResp.data
+    }
+
+    // !!! DOES NOT WORK !!!
+    // async easterEgg(type = 'makeItRain') {
+    //   if (!this.token) throw new Error('This method cannot be used without authorization')
+    //   const easterEggResp = await this.axios.post(`/easter-eggs/${type}`)
+    //   return easterEggResp.data
+    // }
+
+    async searchUsers(query) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const searchUserRes = await this.axios.get(`/users?q=${encodeURIComponent(query)}`)
+        const users = await Promise.all(searchUserRes.data.data.map(async userInfo => {
+            const userRes = await this.getUserById(userInfo.userId)
+            return userRes
+        }))
+
+        return users
+    }
+
+    async getFriends() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const friendsResp = await this.axios.get('/friends')
+        const friends = await Promise.all(friendsResp.data.data.map(async userInfo => {
+            const userRes = await this.getUserById(userInfo.userId)
+            return userRes
+        }))
+        return friends
+    }
+
+    async acceptFriendRequest(userId) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const acceptFriendRes = await this.axios.put(`/friends/${userId}/status`, {
+            status: 'ACCEPTED'
+        })
+        return acceptFriendRes.data.status === 'ACCEPTED'
+    }
+
+    async getUpcomingSchedule() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const leaderboard = await this.axios.get('/shows/schedule')
+        return leaderboard.data.shows
+    }
+
+    async addFriend(userId) {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const addFriendRes = await this.axios.post(`/friends/${userId}/requests`)
+        return addFriendRes.data.status === 'PENDING'
+    }
+
+    async getIncomingFriendRequests() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        const friendsResp = await this.axios.get('/friends/requests/incoming')
+        const friends = await Promise.all(friendsResp.data.data.map(async userInfo => {
+            const userRes = await this.getUserById(userInfo.userId)
+            return userRes
+        }))
+        return friends
+    }
+
+    sendAnswer(answerID, questionId) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'trivia') throw new Error('You can not send a letter because this game is not Trivia')
+        this.WSConn.send(JSON.stringify({
+            questionId: parseInt(questionId),
+            type: 'answer',
+            answerId: parseInt(answerID)
+        }))
+    }
+
+    sendSurveyAnswer(answerID, questionId) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'trivia') throw new Error('You can not send a letter because this game is not Trivia')
+        this.WSConn.send(JSON.stringify({
+            surveyQuestionId: parseInt(questionId),
+            type: 'surveyAnswer',
+            surveyAnswerId: parseInt(answerID),
+            broadcastId: this.broadcastId
+        }))
+    }
+
+    sendEraser(questionId) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'trivia') throw new Error('You can not send a letter because this game is not Trivia')
+        this.WSConn.send(JSON.stringify({
+            type: 'erase1',
+            broadcastId: this.broadcastId,
+            questionId: parseInt(questionId)
+        }))
+    }
+
+    checkpoint(winNow, checkpointId) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'trivia') throw new Error('You can not send a letter because this game is not Trivia')
+        this.WSConn.send(JSON.stringify({
+            type: 'checkpointResponse',
+            broadcastId: this.broadcastId,
+            winNow: winNow,
+            checkpointId: parseInt(checkpointId)
+        }))
+    }
+
+    useExtralife(questionId) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'trivia') throw new Error('You can not send a letter because this game is not Trivia')
+        this.WSConn.send(JSON.stringify({
+            type: 'useExtraLife',
+            questionId: parseInt(questionId)
+        }))
+    }
+
+    sendLetter(roundId, showId, letter) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'words') throw new Error('You can not send a letter because this game is not Words')
+        this.WSConn.send(JSON.stringify({
+            roundId: parseInt(roundId),
+            type: 'guess',
+            showId: parseInt(showId),
+            letter: letter.toUpperCase()
+        }))
+    }
+
+    sendWord(roundId, showId, word) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        if (this.gameType !== 'words') throw new Error('You can not send a letter because this game is not Words')
+        const letters = word.split('')
+        letters.forEach((letter) => {
+            this.sendLetter(roundId, showId, letter)
+        })
+    }
+
+    getErasers(friendIds) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        this.WSConn.send(JSON.stringify({
+            type: 'erase1Earned',
+            broadcastId: this.broadcastId,
+            friendsIds: friendIds
+        }))
+    }
+
+    chatVisibility(enable) {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        this.WSConn.send(JSON.stringify({
+            type: 'chatVisibilityToggled',
+            broadcastId: this.broadcastId,
+            chatVisible: enable
+        }))
+    }
+
+    async connectToGame() {
+        if (!this.token) throw new Error('This method cannot be used without authorization')
+        let shows = {}
+        shows = await this.getShows()
+        if (!shows.active) throw new Error('Game is not active')
+
+        this.WSConn = new WebSocket(shows.broadcast.socketUrl, {
+            headers: this.headers
+        })
+        var pingInterval
+
+        this.WSConn.on('open', () => {
+            pingInterval = setInterval(this.WSConn.ping, 10000)
+            this.broadcastId = parseInt(shows.broadcast.broadcastId)
+            if (shows.nextShowVertical === 'words') {
+                this.gameType = 'words'
+                this.WSConn.send(JSON.stringify({
+                    type: 'subscribe',
+                    broadcastId: shows.broadcast.broadcastId,
+                    gameType: 'words'
+                }))
+            } else {
+                this.gameType = 'trivia'
+            }
+            this.emit('connected', {
+                gameType: this.gameType
+            })
+        })
+
+        this.WSConn.on('close', (code) => {
+            this.emit('disconnected', code)
+            clearInterval(pingInterval)
+        })
+
+        this.WSConn.on('message', (rawData) => {
+            try {
+                const data = JSON.parse(rawData)
+                this.emit('message', data)
+                this.emit(data.type, {
+                    ...data,
+                    lastQuestion: this.lastQuestion
+                })
+                if (data.type === 'question' || data.type === 'startRound') {
+                    this.lastQuestion = data
+                }
+            } catch (e) {
+                this.emit('error', 'Failed parse WS Message')
+            }
+        })
+
+        return this.WSConn
+    }
+
+    async disconnectFromGame() {
+        if (!this.WSConn || this.WSConn.readyState !== WebSocket.OPEN) throw new Error('You are not connected to the game')
+        this.WSConn.close()
+    }
+}
+
+module.exports = HQTrivia
